828bcbb404051c33dfe6e2edeb4dd2eec0d328a3
Diogo <diogo.slopes@innowave.tech>
2023-12-11 11:23:10 +0000

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "dslopes10-test"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+  */
 
   required_providers {
     aws = {
